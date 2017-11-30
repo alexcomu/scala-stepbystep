f7f0c485749817a351740eafa0f0483c0bb7474d
alexcomu <alex.comunian@gmail.com>
2017-11-30 20:08:29 +0100

    renamed 03

--- 03_class_object/compile_me/Counter.scala
+++
@@ -1,6 +0,0 @@
-object Counter{
-    private var c = 0
-    def plus_one(): Unit = { c += 1}
-    def get_counter(): Int = c
-}
-
--- 03_class_object/simple_object.scala
+++
@@ -1,8 +0,0 @@
-// Singleton Objects, it's like a static member
-
-object MyObject {
-    def say_hello(): String = { "Hello!"}
-}
-
-println(MyObject.say_hello())
-
